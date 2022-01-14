ab6629bf19a23c38f65348fe393160de90c8d493
Charles T <tietjen.charles@gmail.com>
2022-01-14 14:01:25 -0500

    filled in questions and some limited logic, need to plan before proceeding

--- index.js
+++ index.js
@@ -1,13 +1,86 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'projectTitle',
+        message: 'What is the title of your project?',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('A title is required.');
+                return false;
+            };
+        }
+    },
+    {
+        type: 'input',
+        name: 'projectDescription',
+        message: 'Enter a description of your project.',
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('A project description is required.');
+                return false;
+            };
+        }
+    },
+    {
+        type: 'input',
+        name: 'screenshotPath',
+        message: 'Enter the path and filename of your project screenshot.'
+    },
+    {
+        type: 'input',
+        name: 'installDescription',
+        message: 'Enter any installation instructions, if applicable.'
+    },
+    {
+        type: 'input',
+        name: 'usageDescription',
+        message: 'Enter any additional usage directions.'
+    },
+    {
+        type: 'confirm',
+        name: 'collabConfirm',
+        message: 'Do you have any collaborators to list?',
+        default: false 
+    },
+    {
+        type: 'input',
+        name: 'collabEntry',
+        message: 'Enter all collaborators seperated by commas.',
+        when: ({ collabConfirm }) => collabConfirm
+    },
+    {
+        type: 'list',
+        name: 'licenseSelect',
+        message: 'Select the license for your project.',
+        choices: ['Apache 2.0', 'Boost 1.0', 'BSD 3', 'CC BY 4.0', 'GNU GPL v3', 'MIT', 'PDDL', 'WTFPL']
+    },
+    {
+        type: 'input',
+        name: 'contributionDescription',
+        message: 'Enter any instructions for future collaborators, if applicable.'
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeToFile('./dist/readme.md', generateMarkdown());
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        writeToFile();
+        // console.log(answers);
+    });
+};
 
 // Function call to initialize app
 init();
---
+++ pseudo.md
@@ -0,0 +1,11 @@
+## Questions Array
+1. Project title
+2. Project description
+3. [optional] Screenshot path
+4. [optional] Languages
+5. [optional] Installation instructions
+6. [optional] Usage information
+7. [optional] Collaborators
+8. [optional] License
+9. [optional] Contribution instructions
+10. [optional] Additional links
